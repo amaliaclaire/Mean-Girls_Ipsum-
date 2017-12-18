ce076456585d88eb53b912b8e951c1e876416dbd
Amalia Claire Fergin <acfergin@msn.com>
2017-12-18 10:04:50 -0800

    able to display paragraphs on page but no can't choose the number of paragraphs, just comes up as 1 paragraph each time

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+function getRandomNum (min, max) {
+  let generateNum = Math.random() * (max - min) + min
 
+  return Math.ceil(generateNum)
+}
 
-function pullQuotes (array) {
-  return array
+function pullQuotes () {
+  let random = getRandomNum(1, 9)
+  let key = 'mgQuotes' + random
+  return meanGirlsObject[key]
 }
 
 pullQuotes()
 
-console.log(mgQuotes2)
+
+  let form = document.getElementById("meangirls-form")
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let mgNumber = document.getElementById('meangirls-number')
+    let text = mgNumber.value
+
+    mgNumber.value = ''
+    mgNumber.focus()
+
+    var newParagraph = document.createElement('div')
+    newParagraph.innerHTML = pullQuotes()
+    
+    let row = document.querySelector('#meangirls-form')
+    row.append(newParagraph)
+  })
