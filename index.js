a6f904f1b9de5c60b67b32a9ad34511a6d1dfb57
Amalia Claire Fergin <acfergin@msn.com>
2017-12-20 09:54:10 -0800

    cleaned up code before surge

--- index.js
+++ index.js
@@ -12,37 +12,30 @@ function pullQuotes () {
 
 pullQuotes()
 
-
   let form = document.getElementById("meangirls-form")
 
   form.addEventListener('submit', function (e) {
     e.preventDefault()
-    // Create meangirls-Number
-    //let meanGirlsParagraphs = document.createElement('div')
-
 
     let mgNumber = document.getElementById('meangirls-number')
-    let text = mgNumber.value
+    let textboxNumber = mgNumber.value
 
     mgNumber.value = ''
     mgNumber.focus()
-    let numTimes = Number(text)
+    let numTimes = Number(textboxNumber)
 
       if (!numTimes) {
         alert("Enter a number and ONLY a number. God Karen you're so stupid")
       }
 
-      // let newParagraph = document.querySelector('.newParagraph-quotes')
       let row = document.querySelector('.mean-girls-container')
       let meanGirlsParagraphs = document.querySelector('#meangirls-paragraph')
 
-      console.log(meanGirlsParagraphs)
       if(meanGirlsParagraphs) {
         meanGirlsParagraphs.innerHTML = ''
       }
 
     for (var i = 0; i < numTimes; i++) {
-        // Adding new Quotes to the page
       newParagraph = document.createElement('div')
       newParagraph.className = "quote"
       newParagraph.innerHTML = pullQuotes()
@@ -51,5 +44,3 @@ pullQuotes()
     }
     row.append(meanGirlsParagraphs)
   })
-
-// loop over the quote divs and remove them one by one.
